bfc4a525b19eeceaedbce0ee879929f6d9ffb4c5
Kuba <jakubryszczyk.inf@gmail.com>
2019-11-05 18:32:46 +0100

    nothing is working as it should

--- config.py
+++ config.py
@@ -4,6 +4,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'] or \
                               'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    conn = psycopg2.connect(SQLALCHEMY_DATABASE_URI, sslmode='require')
+
